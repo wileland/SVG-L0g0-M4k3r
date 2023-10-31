187c023d446b4360e19201f215f5c8ca0e421cdf
wileland <wileland7@gmail.com>
2023-10-30 23:10:34 -0500

    Index.js file- imported modules.

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Triangle = require('./lib/Triangle');
+const Circle = require('./lib/Circle');
+const Square = require('./lib/Square');
